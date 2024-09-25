11c92a778fa08bd415cc13f8bb111ae6216b8a13
jasonnghim8 <jasonlokhim.ng@mail.utoronto.ca>
2024-09-25 00:27:56 +0000

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -12,5 +12,19 @@ public class Driver {
                 System.out.println("1 is a root of s");
         else
                 System.out.println("1 is not a root of s");
+        Polynomial s1 = p1.multiply(p2);
+        System.out.println("s1(1) = " + s1.evaluate(1));
+        Polynomial t0 = p.add(p1);
+        Polynomial t01 = p1.add(p);
+        System.out.println("t0(1) = " + t0.evaluate(1));
+        System.out.println("t01(1) = " + t01.evaluate(1));
+        double[]c3 = {3};
+        Polynomial p3 = new Polynomial(c3);
+        Polynomial t1 = p3.add(p2);
+        System.out.println("t1(1) = " + t1.evaluate(1));
+        Polynomial t2 = p.multiply(p2);
+        System.out.println("t2(1) = " + t2.evaluate(1));
+        Polynomial t3 = p3.multiply(p2);
+        System.out.println("t3(1) = " + t3.evaluate(1));
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,49 +1,168 @@
+import java.io.File;
+import java.util.HashMap;
+import java.nio.file.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+;
+
 public class Polynomial {
     double[] coef;
+    int[] exp;
+    File file;
     
     public Polynomial() {
-        double[] help = {0};
-        coef = help;
+        double[] coef = new double[0];
+        int[] exp = new int[0];
     }    
     
     public Polynomial(double[] input) {
-        coef = input;
+        int z = 0;
+        for (int i = 0; i < input.length; i++){
+            if (input[i] == 0.0) z++;
+        }
+        coef = new double[input.length - z];
+        exp = new int[input.length - z];
+        int k = 0;
+        for (int i = 0; i < input.length; i++){
+            if (input[i] != 0.0){
+                exp[k] = i;
+                this.coef[k] = input[i];
+                k++;
+            }
+        }
     }
-    
+
+   public Polynomial(File input){
+        this.file = input;
+        String lines = "";
+        lines = readFile();
+        String[] var = lines.split("+-");
+        coef = new double[var.length];
+        exp = new int[var.length];
+        String[][] value = new String [var.length][2];
+        for (int i = 0; i < var.length; i++){
+            value[i] = var[i].split("x");
+        }
+        for (int i = 0; i < var.length; i++){
+            coef[i] = Double.parseDouble(value[i][0]);
+            if (value[i].length == 2){
+                exp[i] = Integer.parseInt(value[i][1]);
+            }
+            else exp[i] = 0;
+        }
+    }
+
+    public String readFile(){
+        StringBuilder content = new StringBuilder();
+        try(BufferedReader br = new BufferedReader(new FileReader(file))){
+            content.append(br.readLine());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+
+    public int max(HashMap<Integer, Double> input){
+        int m = 0;
+        for (int i : input.keySet()){
+            if (i > m) m = i;
+        }
+        return m;
+    }
+
+    public Polynomial maptoarray(HashMap<Integer, Double> input){
+        int m = max(input);
+        double[] result = new double[m+1];
+        int i = 0;
+        for (i = 0; i <= m; i++){
+            result[i] = input.getOrDefault(i, 0.0);
+        }
+        return new Polynomial(result);
+    }
+
     public Polynomial add(Polynomial input) {
-        Polynomial result;
-        int len = 0;
-        double s = 0;
-        if (input.coef.length <= this.coef.length){
-            len = this.coef.length;
-            result = this;
-        }
-        else{
-            len = input.coef.length;
-            result = input;
-        }
-        for(int i = 0; i < len; i++){
-            if (i < this.coef.length && i < input.coef.length){
-                s = input.coef[i] + this.coef[i];
-                result.coef[i] = s;
+        HashMap<Integer, Double> sum = new HashMap<>();
+        int c = 0;
+        if (this.coef == null) return input;
+        if (input.coef == null) return this;
+        for(int i = 0; i < this.coef.length; i++){
+            c = this.exp[i];
+            if (!sum.containsKey(c)){
+                sum.put(c, this.coef[i]);
             }
-            else if (i >= input.coef.length){
-                result.coef[i] = this.coef[i];
-            }
-            else result.coef[i] = input.coef[i];
+            else sum.put(c, sum.get(c) + this.coef[i]);
         }
-        return result;
+        for(int i = 0; i < input.coef.length; i++){
+            c = input.exp[i];
+            if (!sum.containsKey(c)){
+                sum.put(c, input.coef[i]);
+            }
+            else sum.put(c, sum.get(c) + input.coef[i]);
+        }
+        return maptoarray(sum);
     }
-    
+
+    public boolean isempty(int[] array){
+        return array.length == 0;
+    }
+
     public double evaluate(double x) {
         double result = 0;
+        if (this.coef == null) return 0;
         for (int i = 0; i < this.coef.length; i++) {
-            result = result + this.coef[i]* Math.pow(x, i);
+            result = result + this.coef[i]* Math.pow(x, this.exp[i]);
         }
         return result;
     }
     
     public boolean hasRoot(double input) {
         return evaluate(input) == 0;
+    }
+
+    public Polynomial multiply(Polynomial input){
+        if (this.coef == null) return this;
+        if (input.coef == null) return input;
+        HashMap<Integer, Double> sum = new HashMap<>();
+        int m = 0;
+        int i = 0;
+        int s = 0;
+        double p = 0;
+        int tel = this.exp.length;
+        int iel = input.exp.length;
+        if (this.exp[tel-1]>=input.exp[iel-1]) m = this.exp[-1];
+        else m = input.exp[iel-1];
+        for (i = 0; i < tel; i++){
+            for (int k = 0; k < iel; k++){
+                s = this.exp[i]+input.exp[k];
+                p = this.coef[i]*input.coef[k];
+                if (!sum.containsKey(s)) sum.put(s, p);
+                else sum.put(s, sum.get(s) + p);
+            }
+        }
+        return maptoarray(sum);
+    }
+
+    public File saveToFile(String name){
+        file = new File(name);
+        String polynomial = "";
+        for (int i = 0; i < this.exp.length; i++){
+            if (this.exp[i] != 0) 
+                polynomial = polynomial + 
+                    Double.toString(this.coef[i]) +
+                        Integer.toString(this.exp[i]);
+            else polynomial = polynomial + 
+                    Double.toString(this.coef[i]);
+        }
+        try{
+            FileWriter writer = new FileWriter(name);
+            writer.write(polynomial);
+            writer.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        return file;
     }
 }
